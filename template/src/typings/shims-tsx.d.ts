b2653198973de3a90d6864b77ec5077538e9bb4b
叶健新 <yejx38448@hundsun.com>
2022-02-28 16:44:42 +0800

    update: 更新模板

---
+++ template/src/typings/shims-tsx.d.ts
@@ -0,0 +1,17 @@
+import Vue, {VNode} from 'vue'
+import { ComponentRenderProxy } from '@vue/composition-api'
+declare global {
+  namespace JSX {
+    type Element = VNode
+    type ElementClass = ComponentRenderProxy
+    interface ElementAttributesProperty {
+      $props: any // specify the property name to use
+    }
+    interface IntrinsicElements {
+      [elem: string]: any
+    }
+  }
+  interface Window {
+    Protocol: any
+  }
+}
